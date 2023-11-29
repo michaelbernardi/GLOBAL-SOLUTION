5f5d31cb921c1124d00d8f7da8be368ebe27b63b
Michael Bernardi <125712660+michaelbernardi@users.noreply.github.com>
2023-11-29 15:39:02 -0300

    Update ConexaoFactory.java

--- SugarInsight3.0/src/main/java/br/com/fiap/conections/ConexaoFactory.java
+++ SugarInsight3.0/src/main/java/br/com/fiap/conections/ConexaoFactory.java
@@ -9,7 +9,7 @@ public class ConexaoFactory {
 	public Connection conexao() throws ClassNotFoundException, SQLException {
 		Class.forName("oracle.jdbc.driver.OracleDriver");
 		
-		return DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl","rm99409","160201");
+		return DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl","","");
 	}
 
 }
